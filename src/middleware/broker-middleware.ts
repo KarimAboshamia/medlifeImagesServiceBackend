3eb7ce8ffbdb90176b9197cfb31c34ccd94066c3
Karim <karimaboshamia@gmail.com>
2023-03-06 16:18:49 +0200

    broker-middleware update

--- src/middleware/broker-middleware.ts
+++ src/middleware/broker-middleware.ts
@@ -2,15 +2,19 @@ import { createChannel, createQueue, generateUrl, deleteImage } from "../utiliti
 
 //get env variable
 const generateUrlQueue = process.env.GENERATE_URLS_QUEUE;
+const deleteImageQueue = process.env.DELETE_IMAGE_QUEUE;
 
 //create function without requests
 export const callReceiver = async () => {
     try {
         console.log("Here")
         const {channel} = await createChannel();
+
         await createQueue(generateUrlQueue, channel);
         generateUrl(generateUrlQueue, channel);
-        deleteImage(generateUrlQueue, channel);
+
+        await createQueue(deleteImageQueue, channel);
+        deleteImage(deleteImageQueue, channel);
 
     } catch (e) {
         console.log(e);
